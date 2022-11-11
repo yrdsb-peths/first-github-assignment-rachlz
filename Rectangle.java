3d241a1726739391c2ec5e9301389917ecbd7876
rachlz <83130998+rachlz@users.noreply.github.com>
2022-11-11 13:05:35 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle
+{
+  private int length; 
+  private int width; 
+  
+  public Rectangle(int len, int wid)
+  {
+     length = len; 
+     width = wid; 
+  }
+  public int getArea()
+  {
+    return length*width; 
+  }
+}
